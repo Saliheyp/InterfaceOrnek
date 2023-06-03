153bde931956406afd41d073db4c6b9431baa728
Saliheyp <m.saliheyupoglu@gmail.com>
2023-06-03 22:02:59 +0300

    first commit

---
+++ Sabitler.cs
@@ -0,0 +1,18 @@
+namespace arayuzler_ornek
+{
+    public enum Marka
+    {
+        Ford,
+        Toyota,
+        Honda
+    }
+    public enum Renk
+    {
+        Beyaz,
+        Gri
+    }
+    // public enum TekerSayisi
+    // {
+
+    // }
+}
